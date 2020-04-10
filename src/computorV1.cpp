10159178aeed514594199136e37b59acfd744d3e
Filip Crnobrnja <aashara.c@gmail.com>
2020-04-10 19:06:19 +0300

    Finish parser for polynom. Close #1

--- src/computorV1.cpp
+++ src/computorV1.cpp
@@ -2,48 +2,47 @@
 
 #include <iostream>
 
-// static size_t	IsValidDelim(const string& delim) {
-// 	size_t	i = 0;
-
-// 	if (delim[i] != '*' && delim[i] != 'x') {
-// 		if (delim[i] != '+' && delim[i] != '-')
-// 			throw invalid_argument("Invalid symbol. It must be +, -, or *. See usage!");
-// 		return (i);
-// 	}
-// 	while (delim[i] == ' ')
-// 		++i;
-// 	if (i != delim.size()) {
-// 		if (delim[i++] != 'x')
-// 			throw invalid_argument("Invalid format. See usage!");
-// 	}
-// 	return (i + 1);
-// }
-
 static double	GetCoeff(const string& str, size_t& i) {
 	int8_t	sign = 1;
 	double	result = 1;
 
-	while (str[i] == ' ')
-		++i;
+	i = str.find_first_not_of(' ', i);
 	if (str[i] == '-' || str[i] == '+') {
 		if (str[i] == '-')
 			sign = -1;
 		++i;
 	}
-	while (str[i] == ' ')
-		++i;
+	i = str.find_first_not_of(' ', i);
 	if (isdigit(str[i])) {
 		size_t	end;
 		result = stod(str.substr(i), &end);
 		i += end;
-	}
+	} else if (str[i] != 'x')
+		throw invalid_argument("Invalid polynom. Expected: digit or 'x'. See usage!");
 	return (result * sign);
 }
 
 static int		GetDegree(const string& str, size_t& i) {
-	(void)str;
-	(void)i;
-	return (0);
+	int	result = 1;
+	i = str.find_first_not_of(' ', i);
+	if (str[i] != '*' && str[i] != 'x')
+		return (0);
+	if (str[i] == '*')
+		++i;
+	i = str.find_first_not_of(' ', i);
+	if (str[i] != 'x')
+		throw invalid_argument("Invalid polynom. Expected: 'x'. See usage!");
+	if (str[++i] == '^') {
+		if (isdigit(str[++i])) {
+			size_t	end;
+			result = stoi(str.substr(i), &end);
+			i += end;
+		}
+		else
+			throw invalid_argument("Invalid polynom. Expected digit after '^'. See usage!");
+
+	}
+	return result;
 }
 
 
@@ -52,19 +51,19 @@ void	ParsePolynom(const string& polynom,
 	size_t	i = 0;
 	int8_t	sign = 1;
 
-	while (i != polynom.size()) {
-		while (polynom[i] == ' ')
-			++i;
-		if (polynom[i] == '=') {
-			sign = -1;
-			continue ;
-		}
+	while (true) {
 		double	coeff = GetCoeff(polynom, i);
 		int		degree = GetDegree(polynom, i);
-		cout << "coeff = " << coeff << endl;
-		cout << "degree = " << degree << endl;
 		deg_to_coeff[degree] += (coeff * sign);
-		break;
+		i = polynom.find_first_not_of(' ', i);
+		if (polynom[i] == '\0')
+			break ;
+		if (polynom[i] == '=') {
+			i = polynom.find_first_not_of(' ', ++i);
+			if (i == polynom.size())
+				throw invalid_argument("Invalid format. No symbols after equals. See usage!");
+			sign = -1;
+		}
 	}
 	if (sign != -1)
 		throw invalid_argument("Invalid format. No symbol equals. See usage!");
